4e789c81e9a08f2e0f631786f1e1119ca29b8bf0
lsh <lshzy137@gmail.com>
2024-10-10 17:53:23 -0400

    updated

---
+++ pass2image.js
@@ -0,0 +1,29 @@
+import process from "process";
+import mosaic from "./mosaicVisualHash.js"
+import jsSHA from "jssha";
+import minimist from "minimist";
+import { createCanvas } from "canvas";
+import fs from "fs";
+
+function redrawCanvas(password, canvas, params) {
+    var shaObj = new jsSHA("SHA-1", "TEXT");
+    shaObj.update(password);
+    var hash = shaObj.getHash("BYTES");
+    console.log(hash)
+    mosaic.display(hash, canvas, params)
+}
+
+var argv = minimist(process.argv.slice(2), {
+    string: ['password'],
+    default: { 'jitter': 3, 'output': './images/test.png' }
+});
+console.log(argv)
+
+const canvas = createCanvas(256, 256)
+
+redrawCanvas(argv.password, canvas, { jitter: argv.jitter })
+
+const out = fs.createWriteStream(argv.output);
+const stream = canvas.createPNGStream();
+stream.pipe(out);
+out.on('finish', () => console.log('The PNG file was created.'))
